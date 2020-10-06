5ae8a803eb0c9ce385f86a143987ad3ec013803b
E. Lynette Rayle <elr37@cornell.edu>
2020-10-06 15:27:26 -0400

    add debugging statement for command to check if MySQL is running
    check command with…
    ```
    docker-compose logs | grep 'nc -z'
    ```

--- bin/db-wait.sh
+++ bin/db-wait.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
 # Wait for MySQL
+CMD="nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT"
+echo "-----------------------------------"
+echo " "
+echo "bin/db-wait.sh -- The following command will run to check if MySQL is running..."
+echo "bin/db-wait.sh -- $CMD"
+echo " "
+echo "-----------------------------------"
 until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT; do
  echo 'Waiting for MySQL...'
  sleep 1
