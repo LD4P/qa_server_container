b3461f372928dfb38f766ded94d1c3f1ca77b989
E. Lynette Rayle <elr37@cornell.edu>
2022-04-21 16:24:53 -0400

    use newer env vars

--- bin/db-wait.sh
+++ bin/db-wait.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
 # Wait for MySQL
-CMD="nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT"
+# CMD="nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT"
+CMD="nc -z -v -w30 $MYSQL_HOST $MYSQL_PORT"
 echo "-----------------------------------"
 echo " "
 echo "bin/db-wait.sh -- The following command will run to check if MySQL is running..."
 echo "bin/db-wait.sh -- $CMD"
 echo " "
 echo "-----------------------------------"
-until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT; do
+# until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT; do
+until nc -z -v -w30 $MYSQL_HOST $MYSQL_PORT; do
  echo 'Waiting for MySQL...'
  sleep 1
 done
